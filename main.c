4c2f74403c8a4e594b3ed342ada20e75717245bd
joaocm1804 <joaocarlosmendes18@gmail.com>
2025-05-14 12:11:25 -0300

    Aplicação dos movimentos do personagem

--- main.c
+++ main.c
@@ -13,26 +13,30 @@ typedef struct Player{
     Vector2 position;
     Texture2D texture;
     int vida;
+    int speed;
     Inventario backpack;
 } Player;
 
 
 typedef struct Bullet{
     float speed;
-    Texture2D texture;
     Vector2 position;
-    int damage;
-    bool ativo;
+    Vector2 direction;
+    //int damage;
+    //bool ativo;
+    struct Bullet * next;
 } Bullet;
 
-
+//Inicializa texturas
 static Texture2D background;
 static Texture2D personagem;
+static Texture2D bullet;
 
 // Variaveis Globais:
 static const int screenWidth = 1280;
 static const int screenHeight = 720;
-static int player_speed = 7;
+
+//
 
 static bool gameOver = false;
 static bool pause = false;
@@ -41,13 +45,13 @@ static bool victory = false;
 static Player player = { 0 };
 //static Bullet bullet[tamanho] = { 0 }; //ajustar
 
-static int destroyedBulletsCount = 0;
+static float tempo_decorrido = 0;
 
 static void InitGame(void);         // Initialize game
 static void UpdateGame(void);       // Update game (one frame)
 static void DrawGame(void);         // Draw game (one frame)
 static void UnloadGame(void);       // Unload game
-static void UpdateDrawFrame(void);  // Update and Draw (one frame)
+
 
 
 int main(void)
@@ -77,9 +81,15 @@ void InitGame(void){
 
     //Carregar os sprites as suas respectivas funções
     background = LoadTexture("assets/background.png");
-    personagem = LoadTexture("assets/Ruhtra.png");
+    personagem = LoadTexture("assets/Unarmed_Idle_full.png");
+    bullet = LoadTexture("assets/bullet.png");
 
+    //inicializa as variáveis do player
     player.position = (Vector2){screenWidth - 40, screenHeight - 50};
+    player.vida = 3;
+    player.speed = 7;
+    player.backpack.madeira = 0;
+    player.backpack.pedra = 0;
     
 }
 
@@ -107,14 +117,20 @@ void DrawGame(void){
 }
 
 void UpdateGame(void){
-    if (IsKeyDown(KEY_A)){
-        player.position.x -= player_speed;
-    } else if (IsKeyDown(KEY_D)){
-        player.position.x += player_speed;
-    } else if (IsKeyDown(KEY_W)){
-        player.position.y -= player_speed;
-    } else if (IsKeyDown(KEY_S)){
-        player.position.y += player_speed;
+    //inicialização do tempo para aumentar dificuldade
+    float deltaTime = GetFrameTime();
+    tempo_decorrido += deltaTime; 
+
+
+
+    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)){
+        player.position.x -= player.speed;
+    } else if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)){
+        player.position.x += player.speed;
+    } else if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)){
+        player.position.y -= player.speed;
+    } else if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)){
+        player.position.y += player.speed;
     }
 }
 
